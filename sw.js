880122b0a09f8339fb235a18c1d9daf3fb0d26fe
sulimanalhedary <s_alhedary@yahoo.com>
2026-09-11 21:20:46 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='alziyad-calendar-v4';
+const CACHE='alziyad-calendar-v5';
 const CORE=['./','./index.html','./manifest.webmanifest','./logo.jpg','./icon-192.png','./icon-512.png','./assets/riyadh-city.jpg','./assets/riyadh-work.jpg'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)))});
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith('alziyad-calendar-')&&k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
